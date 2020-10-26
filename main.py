bbac5ec558bac2e9bb24f9f7b08a06f9255c0549
Maxime "Napolitain" Boucher <mxboucher@gmail.com>
2020-10-26 23:13:00 +0100

    main -> object statique App

--- main.py
+++ main.py
@@ -5,37 +5,34 @@ import Partie
 import Constants
 from Enumerations import Status
 
-window = tkinter.Tk()
+class App(object):
 
-FPS = tkinter.IntVar()
-FPS.set(1)
+    window = tkinter.Tk()
+    FPS = tkinter.IntVar()
+    background = tkinter.Canvas(window, width=Constants.t, height=Constants.t, background="#000", bd=0, highlightthickness=0)
+    controls = tkinter.Toplevel()
+    statusPartie = tkinter.StringVar()
+    labelPartie = tkinter.Label(controls, textvariable=statusPartie)
+    buttonPartie = tkinter.Button(controls, command=Partie.start)
+    labelVitesse = tkinter.Label(controls, text="Vitesse:")  # modifier vitesse
+    scaleVitesse = tkinter.Scale(controls, orient=tkinter.HORIZONTAL, from_=1, to=60, length=180, variable=FPS, cursor='sb_h_double_arrow')  # Curseur pour modifier les FPS
 
-window.title("Pac Man")
-window.geometry(str(Constants.t) + "x" + str(Constants.h))
-window.resizable(width=False, height=False)
+    @staticmethod
+    def initialize():
+        App.FPS.set(1)
+        App.window.title("Pac Man")
+        App.window.geometry(str(Constants.t) + "x" + str(Constants.h))
+        App.window.resizable(width=False, height=False)
+        App.background.pack()
+        App.controls.resizable(width = False, height = False)
+        App.controls.title("Contrôles")
+        App.controls.geometry("200x350")
+        App.statusPartie.set(Status.pause.value)
+        Partie.initialize()
+        App.labelPartie.pack()
+        App.buttonPartie.pack()
+        App.labelVitesse.pack()
+        App.scaleVitesse.pack()
+        App.window.mainloop()
 
-background = tkinter.Canvas(window, width=Constants.t, height=Constants.t, background="#000", bd=0, highlightthickness=0)
-background.pack()
-
-controls = tkinter.Toplevel()
-
-controls.resizable(width = False, height = False)
-controls.title("Contrôles")
-controls.geometry("200x350")
-
-statusPartie = tkinter.StringVar()
-statusPartie.set(Status.pause.value)
-
-Partie.initialize()
-
-labelPartie = tkinter.Label(controls, textvariable=statusPartie)
-buttonPartie = tkinter.Button(controls, command=Partie.start)
-labelVitesse = tkinter.Label(controls, text="Vitesse:") # modifier vitesse
-scaleVitesse = tkinter.Scale(controls, orient=tkinter.HORIZONTAL, from_=1, to=60, length=180, variable=FPS, cursor='sb_h_double_arrow') # Curseur pour modifier les FPS
-
-labelPartie.pack()
-buttonPartie.pack()
-labelVitesse.pack()
-scaleVitesse.pack()
-
-window.mainloop()
+App.initialize()
